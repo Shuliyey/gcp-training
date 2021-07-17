ea6208a4f7c4d5168a70e57311d35c84aa20f18e
叶泽宇 <shuliyey@163.com>
2021-07-17 16:59:02 +1000

    :sparkles: add `permissions` `streaming-processing-with-pubsub-and-dataflow`

--- qwiklabs/stream-processing-with-pubsub-and-dataflow/permissions.ps1
+++ qwiklabs/stream-processing-with-pubsub-and-dataflow/permissions.ps1
@@ -3,3 +3,5 @@ project=${PROJECT:-project-xxxxxxx}
 
 gcloud projects add-iam-policy-binding --member="user:${user}" --role="roles/pubsub.admin" ${project}
 gcloud projects add-iam-policy-binding --member="user:${user}" --role="roles/cloudscheduler.admin" ${project}
+gcloud projects add-iam-policy-binding --member="user:${user}" --role="roles/monitoring.admin" ${project}
+gcloud projects add-iam-policy-binding --member="user:${user}" --role="roles/appengine.appCreator" ${project}
